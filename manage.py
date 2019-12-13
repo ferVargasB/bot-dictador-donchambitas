b27fd3286a3a90304ccd6874528c702a948fef78
ferVargasB <f.vargas.bravo@icloud.com>
2019-12-13 14:22:43 -0600

    prueba de ruta raíz
    Se añadió una ruta / , para ver un mensaje al visitarla.

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
 from app import create_app
 from flask_script import Manager
+from flask import request
 
 
 app = create_app()
+
+
+@app.route('/')
+def home():
+    print(request)
+    return f'Esta app correo en: {request.host}'
 
 if __name__ == "__main__":
     manager = Manager(app)
